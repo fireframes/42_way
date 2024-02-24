73eeaf25fe11de70f000a2b64efc3ae7c49eaf51
fireframes <82419815+fireframes@users.noreply.github.com>
2024-02-25 02:22:28 +0700

    Update libft.h

--- libft.h
+++ libft.h
@@ -46,9 +46,8 @@ char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
 char	**ft_split(char const *s, char c);
-// 3 more to go...
-
-
+char	*ft_itoa(int n);
+// 2 more to go...
 void	ft_putchar_fd(char c, int fd);
 void	ft_putstr_fd(char *s, int fd);
 void	ft_putendl_fd(char *s, int fd);
